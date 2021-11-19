ea288048052fe61ae9d282432999b8576296ccbc
Azonkeu <mazonkeu@gmail.com>
2021-11-19 09:20:32 +0100

    Add the javascript code for the form validation

--- myscript.js
+++ myscript.js
@@ -24,3 +24,26 @@ const contact = document.getElementById('contactpage');
 contact.onclick = function () {
   modal.style.display = 'none';
 };
+
+const forms = document.getElementById("getForm");
+const msg = document.getElementById("small");
+const success = document.getElementById("success");
+const email = document.getElementById("email");
+
+function checkEmail(email) {
+  return email === email.toLowerCase();
+}
+
+forms.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (checkEmail(email.value)) {
+    msg.classList.add("showMessage");
+    success.classList.remove("showSuccess");
+    forms.submit();
+  } else {
+    msg.textContent = "Please remove any uppercase!";
+    msg.classList.remove("showMessage");
+    success.classList.add("showSuccess");
+    success.textContent = "success";
+  }
+});
