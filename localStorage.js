d7b0d23b15e30e61ce8b198bcafd67ad4b95ba45
oluwajuwon8 <azeezatlola@gmail.com>
2021-11-19 18:59:05 +0100

    Add javascript for local storage

--- localStorage.js
+++ localStorage.js
@@ -16,3 +16,27 @@ names.addEventListener("input", function(e) {
 
     localStorage.setItem("userInfo", JSON.stringify(userInfo));
 });
+
+emails.addEventListener("input", function(e) {
+    e.preventDefault();
+
+    userInfo.Email = emails.value;
+
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+});
+text.addEventListener("input", function(e) {
+    e.preventDefault();
+
+    userInfo.Message = text.value;
+
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+});
+
+const data = JSON.parse(localStorage.getItem("userInfo"));
+userInfo.Name = data.Name;
+userInfo.Email = data.Email;
+userInfo.Message = data.Message;
+
+text.value = userInfo.Message;
+emails.value = userInfo.Email;
+names.value = userInfo.Name;
